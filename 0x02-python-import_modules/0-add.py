b2c660b557cbcd1e978cbb7b63f690e64a18e7c0
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-03-15 00:48:23 +0000

    updated simple add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print(f"{a} + {b} = {add_0.add(a, b)}")
-
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
