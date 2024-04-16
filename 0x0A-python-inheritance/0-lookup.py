1a6deeb176715b78b527cc3ca79f1f342c4d55b6
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-04-16 02:32:48 +0000

    edited+ lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Defines a lookup function"""
 
 def lookup(obj):
     """
     Return a list of obj attrributes
-
     obj: an instant of a class
     """
-    return dir(obj)
+    return (dir(obj))
