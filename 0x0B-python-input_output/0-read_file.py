8a61bab2910bca571795de4e332bb37140f2b661
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-04-17 04:08:16 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that read a text file and print it to stdout"""
+
+def read_file(filename=""):
+    """Read text file and print to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        data = f.read()
+        print(data)
