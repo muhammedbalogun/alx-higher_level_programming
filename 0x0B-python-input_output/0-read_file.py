3d2fa0e65e859bf834cbb73eba1ddeb6deeb419d
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-04-17 04:18:37 +0000

    edited read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """Read text file and print to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         data = f.read()
-        print(data)
+        print(data, end="")
